90e0eac4f81ef8762133e16daef1bcbc86fec463
Gracrys <gracrys@gmail.com>
2020-08-03 19:49:49 -0430

    fetch for passing down credentials and confirmations

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,32 @@
 const Koa = require('koa'),
   route = require('koa-route'),
-  websockify = require('koa-websocket');
+  websockify = require('koa-websocket'),
+  cors = require('@koa/cors'),
+  bP = require('koa-bodyparser');
 
 const app = websockify(new Koa());
+app.use(cors())
+app.use(bP())
 
-const clients = []
+let games = {}
+let clients = []
+app.use( route.all('/', function(ctx, next){
+      console.log(ctx.request.body) 
+      next(ctx)
+      ctx.body = ctx.request.body
+    }
+  )
+)
 
-app.ws.use(route.all('/:id', function(ctx, next) {
+app.ws.use(route.all('/:id', function(ctx, next, id) {
   // return `next` to pass the context (ctx) on to the next ws middleware
   // clients.push(connection)
+  games[id] = "11"
   clients.push(ctx)
   ctx.websocket.on('message', function(message) {
   	clients.forEach(c => c.websocket.send(message))
     // do something with the message from client
-    console.log(message)
+    console.log(message, id, next)
   });
   return next(ctx);
 }));
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,40 @@ import { technique, jutsus } from "./jutsus.ts"
 const footerLog = document.querySelector("footer #logger")
 const symbol = document.querySelector("main .js-symbol")
 const symbolDesc = document.querySelector("main .js-symbol_desc")
-
+const ip = "10.10.1.122:3000"
 
 window.onload = e =>{
+
+
 	if(window.location.pathname === "/"){
+		async function postData(url = '', data = {}) {
+		  // Default options are marked with *
+		  const response = await fetch(url, {
+		    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+		    mode: 'cors', // no-cors, *cors, same-origin
+		    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+		    // credentials: 'include', // include, *same-origin, omit
+		    headers: {
+		      'Content-Type': 'application/json',
+		      // 'Content-Type': 'application/x-www-form-urlencoded',
+		       "Accept": "application/json",
+		    },
+		    // redirect: 'follow', // manual, *follow, error
+		    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		    body: JSON.stringify(data) // body data type must match "Content-Type" header
+		  });
+		  return response.json(); // parses JSON response into native JavaScript objects
+		}
+
+		postData('http://'+ip, { answer: 42 })
+		  .then(data => {
+		    console.log(data); // JSON data parsed by `data.json()` call
+		  }.catch( x => console.warn("fuck");		
 		console.warn("modal")	
 	}
 }
 
-const ws = new WebSocket("ws://10.10.1.122:3000/21");
+const ws = new WebSocket("ws://"+ip+"/21");
 ws.onmessage = x => console.warn(x.data)
 
 console.log(jutsus)
